4f0ae2c721c0de8a22c9fd8574d763c86af76715
Jean-Michel Bakker <jmb@macbook13.local>
2020-12-09 13:40:48 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,23 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+
+import { HomePage } from '../pages/home/home';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
-export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+export class MyApp {
+  rootPage = HomePage;
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { BrowserModule } from '@angular/platform-browser';
+import { Camera } from '@ionic-native/camera';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  declarations: [
+    MyApp,
+    HomePage
   ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, IonicModule.forRoot()],
+  bootstrap: [MyApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    Camera
+  ]
 })
 export class AppModule {}
